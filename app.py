b6551de6cac69d1235e8ae65ca576257c6afdd59
mohammad mohiuddin mostafa kamal akib <mohiuddinmostafakamal@gmail.com>
2019-02-25 01:36:09 +0600

    Modified: added flask-migration to elegantly handle db migrations from terminal, created models and connected with postgreSQL

--- app.py
+++ app.py
@@ -1,41 +1,66 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from datetime import datetime
+import os
 
-
+# Initi app
 app = Flask(__name__)
+# App secret key
+app.config['SECRET_KEY'] = os.environ.get(
+    'SECRET_KEY') or '7cb40e16074e4222e8ee8a99e9364353'
 # Csrf token
 csrf = CSRFProtect(app)
+# DB uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URI') or 'postgresql://postgres:akib123@localhost/flaskblog'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+#     'DATABASE_URI') or "postgresql://root:''@localhost/flaskblog"
+# DB instance
+db = SQLAlchemy(app)
+# DB migration handler
+migrate = Migrate(app=app, db=db)
 
-# Secret key
-app.config['SECRET_KEY'] = '7cb40e16074e4222e8ee8a99e9364353'
 
-# posts
-posts = [
-    {
-        'author': 'Md akib',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'Feb 24, 2019'
-    },
-    {
-        'author': 'Md alif',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'Feb 27, 2019'
-    },
-    {
-        'author': 'Mr jane',
-        'title': 'Blog post 3',
-        'content': 'Third post content',
-        'date_posted': 'Jan 11, 2019'
-    },
-]
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    image_file = db.Column(db.String(20), default='default.jpg', nullable=True)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+    def __str__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+# Post model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+    def __str__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 # Home page
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -58,11 +83,6 @@ def login():
             return redirect(url_for('login'))
     return render_template('login.html', form=form, title="Login")
 
-# 404 page
-@app.errorhandler(404)
-def page_not_found(error):
-    return error
-
 
 # Registration route
 @app.route('/register', methods=['GET', 'POST'])
@@ -72,6 +92,12 @@ def register():
         flash(f"Account created for {form.username.data}", 'success')
         return redirect(url_for('home'))
     return render_template('register.html', form=form)
+
+
+# 404 page
+@app.errorhandler(404)
+def page_not_found(error):
+    return error
 
 
 if __name__ == "__main__":
